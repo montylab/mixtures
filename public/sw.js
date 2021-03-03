4ef95997b86397d74559421cde218e7cf4a52d1c
Eduard Kovalenko <e.kovalenko@verv.com>
2021-03-04 01:35:03 +0300

    pwa

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,32 @@
-const KEY = 'mixtures-cache-v1';
+const KEY = 'mixtures-cache-v1'
 
 self.addEventListener('install', (event) => {
-	event.waitUntil(self.skipWaiting());
-});
+	event.waitUntil(self.skipWaiting())
+})
 
 self.addEventListener('message', (event) => {
 	if (event.data.type === 'CACHE_URLS') {
 		event.waitUntil(
 			caches.open(KEY)
 				.then( (cache) => {
+					console.log(event.data.payload)
 					return cache.addAll(event.data.payload);
 				})
 		);
 	}
-});
+})
+
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.open(KEY).then(cache => {
+			fetch(event.request).then((response) => {
+				if (event.request.method === 'GET') {
+					cache.put(event.request, response.clone())
+				}
+				return response.clone()
+			}).catch()
+
+			return caches.match(event.request)
+		})
+	)
+})
