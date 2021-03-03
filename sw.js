75da511c2958928f43b7c056694bb87f306c8e93
montylab <montylabru@gmail.com>
2021-03-03 20:50:23 +0000

    Deploying to pages from @ montylab/mixtures@981f2fd8cdc1012e50e2340046611080b9883112 🚀

---
+++ sw.js
@@ -0,0 +1,16 @@
+const KEY = 'mixtures-cache-v1';
+
+self.addEventListener('install', (event) => {
+	event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('message', (event) => {
+	if (event.data.type === 'CACHE_URLS') {
+		event.waitUntil(
+			caches.open(KEY)
+				.then( (cache) => {
+					return cache.addAll(event.data.payload);
+				})
+		);
+	}
+});
